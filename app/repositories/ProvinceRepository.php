4eeb56b4931760c560a9543c7b260db56749bf12
KianHmz <hmz00abd@gmail.com>
2025-05-01 14:46:01 +0330

    add province  method: READ

---
+++ app/repositories/ProvinceRepository.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Core\Database;
+use App\Utilities\QueryHelper;
+
+class ProvinceRepository
+{
+    protected $db;
+    protected $table = 'province';
+
+    public function __construct()
+    {
+        $this->db = Database::connect();
+    }
+
+    public function getProvinces(array $data = []): array
+    {
+        $fields = QueryHelper::fieldsFilter($data, $this->table);
+        $sql = "SELECT $fields FROM {$this->table}";
+
+        $ordering = QueryHelper::orderbyFilter($data, $this->table);
+        if (!empty($ordering)) {
+            $sql .= " $ordering";
+        }
+
+        $pagination = QueryHelper::paginationFilter($data);
+        if (!empty($pagination)) {
+            $sql .= " $pagination";
+        }
+
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll(\PDO::FETCH_OBJ);
+    }
+}
