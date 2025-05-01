d8fce4876c1ae595932677c4104e5795e055b544
KianHmz <hmz00abd@gmail.com>
2025-05-01 16:17:27 +0330

    optimize core

---
+++ app/core/config.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'db' => [
+        'host' => 'localhost',
+        'name' => 'iran2',
+        'username' => 'root',
+        'password' => '',
+    ],
+    'cache' => [
+        'enabled' => true,
+        'exp' => 3600, // 1 hour
+        'dir' => __DIR__ . '/../cache',
+    ],
+];
--- bootstrap.php
+++ bootstrap.php
@@ -1,8 +1,13 @@
 <?php
 
 require_once __DIR__ . '/vendor/autoload.php';
+$config = require __DIR__ . '/core/config.php';
 
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'iran2');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_HOST', $config['db']['host']);
+define('DB_NAME', $config['db']['name']);
+define('DB_USERNAME', $config['db']['username']);
+define('DB_PASSWORD', $config['db']['password']);
+
+define('CACHE_ENABLED', $config['cache']['enabled']);
+define('CACHE_EXPIRE_TIME', $config['cache']['dir']);
+define('CACHE_DIR', $config['cache']['exp']);
